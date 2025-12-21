18e12d1242ab580b4f571e216768aa7cf6c35920
XolHed3RP <105742572+arielai91@users.noreply.github.com>
2025-12-21 13:43:32 -0500

    feat: add PostgreSQL support with TypeORM and configuration management
    - Added @nestjs/config and @nestjs/typeorm dependencies for configuration and database integration.
    - Configured TypeORM to connect to a PostgreSQL database using environment variables.
    - Created a .env.template file for database configuration.
    - Updated CommonModule to include an empty imports array for future enhancements.

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,24 @@ import { UsersModule } from './modules/users/users.module';
 import { VerificationModule } from './modules/verification/verification.module';
 import { AuditModule } from './modules/audit/audit.module';
 import { CommonModule } from './modules/common/common.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: String(process.env.DB_PASSWORD),
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: false,
+    }),
     AuthModule,
     UsersModule,
     VerificationModule,
